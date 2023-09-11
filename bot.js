9beba49fab39aea49a79b419dd21f66282627160
Alejandro Sojo <alejandrojsrv@gmail.com>
2023-09-11 00:33:43 -0400

    guardar historial y lloger

--- bot.js
+++ bot.js
@@ -16,9 +16,10 @@ class ChatBot {
         logger.info(`initializeSession:  ${userName}`);
         const llm = new ChatOpenAI({ modelName: 'gpt-3.5-turbo', temperature: 0, openAIApiKey: process.env.OPEN_AI_KEY, topP: 0.3 });
         const savedMemory = await this.db.loadMemory(userName);
-        const memory = new ConversationSummaryMemory({ memoryKey: 'chat_history', llm, chatHistory: JSON.parse(savedMemory) });
-
+        logger.info(`savedMemory ${savedMemory}`);
         logger.info(`chat_history:  ${JSON.parse(savedMemory)}`);
+        
+        const memory = new ConversationSummaryMemory({ memoryKey: 'chat_history', llm, chatHistory: JSON.parse(savedMemory) });
         const promptBasic = await this.db.getPrompt(this.business);
         const prompt = PromptTemplate.fromTemplate(`${promptBasic}\nCurrent conversation:\n{chat_history}\nHuman: {input}\nAI:`);
         const chain = new LLMChain({ llm, prompt, memory });
@@ -36,7 +37,7 @@ class ChatBot {
 
         // Actualizar el historial de conversación
         session.memory.setMemoryVariables({ chat_history: session.memory.getMemoryVariables().chat_history + `Human: ${input}\nAI: ${res}\n` });
-        
+
         const memoryData = session.memory.getMemoryVariables();
         logger.info(`update chat_history:  ${JSON.stringify(memoryData)}`);
         await this.db.saveMemory(userName, JSON.stringify(memoryData));
